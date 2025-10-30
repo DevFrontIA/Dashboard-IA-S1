e276d0ce00dedb5c9c719ccd3d7acb756cc03395
DevFrontIA <devfrontia@gmail.com>
2025-10-30 05:40:53 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from 'react';
+import ChatContainer from './components/ChatContainer'; 
+
+function App() {
+  return (
+    <div className="App-Dashboard">
+      <ChatContainer />
+    </div>
+  );
+}
+
+export default App;
